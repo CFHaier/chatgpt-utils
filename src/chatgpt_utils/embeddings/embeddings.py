4312b5862159b0f3ae142dcd2445ae02644616d6
CFHaier <tancf@haiersingapore.com>
2023-10-27 09:42:01 +0800

    nj

--- src/chatgpt_utils/embeddings/embeddings.py
+++ src/chatgpt_utils/embeddings/embeddings.py
@@ -10,3 +10,12 @@ def get_hf_embeddings(texts, model_name: str, token: str):
     HEADERS = {"Authorization": f"Bearer {token}"}
     response = requests.post(HF_EMBEDDING_API_URL, headers=HEADERS, json={"inputs": texts, "options":{"wait_for_model":True}})
     return response.json()
+
+
+def get_openai_embeddings(texts, model_name, token):
+    HEADERS = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    URL = "https://api.openai.com/v1/embeddings"
+    response = requests.post(URL, headers=HEADERS, json={"input": texts, "model":model_name, "encoding_format": "float"}).json()
+    if len(response['data']) == 1:
+        return response['data'][0]['embedding']
+    return list(map(lambda element:element['embedding'], response['data']))
